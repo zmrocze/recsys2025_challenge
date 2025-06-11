c1e4ed62adaa79f197eb97dbc188614d01daa22d
zmrocze <karolochmanmilarski@gmail.com>
2025-06-12 01:33:36 +0200

    export device

--- src/our_lib/__init__.py
+++ src/our_lib/__init__.py
@@ -4,4 +4,4 @@ from recsys_cmds import split_data, create_baseline_embeddings, contest_training
 
 from recsys_data import RecSysData
 
-from gat import NodeIdMap, JustGAT, RecGAT, DotproductEdgePredictor, LinearEdgePredictor, BprLossLoader, test_out, l2_reg, train_test_split_pos_edges, int_tensor, unique_edges, loss_f, create_target_from_edge_index, BprTraining
+from gat import NodeIdMap, JustGAT, RecGAT, DotproductEdgePredictor, LinearEdgePredictor, BprLossLoader, test_out, l2_reg, train_test_split_pos_edges, int_tensor, unique_edges, loss_f, create_target_from_edge_index, BprTraining, device
