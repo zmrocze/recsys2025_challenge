59005637ee07acb6aa82e8bd3e9ad22b7ab2d97b
zmrocze <karolochmanmilarski@gmail.com>
2025-06-15 19:45:41 +0200

    fix dotpredictor nans

--- src/our_lib/gat.py
+++ src/our_lib/gat.py
@@ -250,6 +250,8 @@ class DotproductEdgePredictor(torch.nn.Module):
     super(DotproductEdgePredictor, self).__init__()
     self.Ws = torch.nn.Parameter(torch.Tensor(emb_dim, emb_dim)).to(device=device)
     self.Wt = torch.nn.Parameter(torch.Tensor(emb_dim, emb_dim)).to(device=device)
+    self.Ws.xavier_uniform_()
+    self.Wt.xavier_uniform_()
     
   def forward(self, user_emb, item_emb):
     """
